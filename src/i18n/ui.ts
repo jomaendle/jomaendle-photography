ea7be93511f61667b4755febb0d793b31876c509
Johannes Maendle <141119154+jomaendle2@users.noreply.github.com>
2024-02-09 09:02:28 +0100

    i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  en: "English",
+  de: "Deutsch",
+};
+
+export const defaultLang = "de";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+    "nav.contact": "Contact",
+  },
+  de: {
+    "nav.home": "Home",
+    "nav.about": "Über",
+    "nav.contact": "Kontakt",
+  },
+} as const;
