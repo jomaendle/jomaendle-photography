dc5c572bb813e113c2c2089694dc314a6dfb887b
Johannes Maendle <141119154+jomaendle2@users.noreply.github.com>
2024-02-11 22:57:06 +0100

    fix try

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,4 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
-    i18n: {
-        defaultLocale: "de",
-        locales: ["en", "de"],
-        routing: {
-            prefixDefaultLocale: true,
-        }
-    }
 });
