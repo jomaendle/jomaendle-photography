35e23fd5ab8f53e2f7eb5fea6c37e6e0416be07d
Johannes Maendle <141119154+jomaendle2@users.noreply.github.com>
2024-03-05 07:26:20 +0100

    format

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,21 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content'
+
+export const portraitImagesKey = 'portrait-images' as const
+export const landscapesKey = 'landscapes' as const
 
 // 2. Define your collection(s)
 const imageCollection = defineCollection({
-  schema: ({ image }) =>
-    z.object({
-      image: image(),
-      category: z.string().optional(),
-    }),
-});
+	schema: ({ image }) =>
+		z.object({
+			image: image(),
+			category: z.string().optional()
+		})
+})
 
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  ["portrait-images"]: imageCollection,
-  landscapes: imageCollection,
-};
+	[portraitImagesKey]: imageCollection,
+	[landscapesKey]: imageCollection
+}
