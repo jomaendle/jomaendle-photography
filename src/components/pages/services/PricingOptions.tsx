db0b584934eb27c374a35ab890b170ed55d90646
Johannes Maendle <141119154+jomaendle2@users.noreply.github.com>
2024-06-20 09:03:17 +0200

    remove framer motion

--- src/components/pages/services/PricingOptions.tsx
+++ src/components/pages/services/PricingOptions.tsx
@@ -5,6 +5,7 @@ import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group.tsx';
 import { useEffect, useState } from 'react';
 import { CameraIcon, ImageIcon } from 'lucide-react';
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card.tsx';
+import type { Language } from '@/i18n/utils.ts';
 
 export interface PricingOptionsTranslations {
 	shooting: string;
@@ -16,11 +17,13 @@ export default function PricingOptions({
 	subtitle,
 	shootings,
 	translations,
+	lang,
 }: {
 	title: string;
 	subtitle: string;
 	translations: PricingOptionsTranslations;
 	shootings: PhotoShootingOffersTranslated[];
+	lang: Language;
 }) {
 	const [selectedShooting, setSelectedShooting] = useState<'shooting' | 'editing'>('shooting');
 	const [shootingOffers, setShootingOffers] = useState<PhotoShootingOffersTranslated[]>(
@@ -30,6 +33,10 @@ export default function PricingOptions({
 	useEffect(() => {
 		setShootingOffers(shootings.filter((shooting) => shooting.type === selectedShooting));
 	}, [selectedShooting]);
+
+	function onShootingClick(shooting: PhotoShootingOffersTranslated) {
+		console.log(shooting);
+	}
 
 	return (
 		<div className="grid w-full items-center gap-6 md:mx-auto md:max-w-5xl lg:gap-12">
@@ -58,7 +65,10 @@ export default function PricingOptions({
 
 			<div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
 				{shootingOffers.map((shooting, index) => (
-					<Card className={`flex cursor-pointer flex-col gap-4 overflow-hidden`} key={index}>
+					<Card
+						className={`flex cursor-pointer flex-col gap-4 overflow-hidden transition-all hover:scale-[1.02] hover:shadow-lg`}
+						key={index}
+					>
 						<CardHeader className="bg-gray-50 p-6">
 							<h3 className="text-xl font-bold">{shooting.title}</h3>
 							<p className="mt-1 text-sm text-gray-500">For {shooting.targetGroup}</p>
@@ -78,9 +88,16 @@ export default function PricingOptions({
 							</ul>
 						</CardContent>
 						<CardFooter>
-							<Button variant="outline" size="lg" className={'w-full'}>
-								{shooting.cta}
-							</Button>
+							<a href={`/${lang}/contact&offer=${shooting.title}`}>
+								<Button
+									variant="outline"
+									size="lg"
+									className={'w-full'}
+									onClick={() => onShootingClick(shooting)}
+								>
+									{shooting.cta}
+								</Button>
+							</a>
 						</CardFooter>
 					</Card>
 				))}
